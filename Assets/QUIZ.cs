1cfa025271069fc5afa8901376ee1ae47920b506
marcos <marcosfeitosapalmas@gmail.com>
2024-01-20 12:09:10 -0300

    first commit

---
+++ Assets/QUIZ.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QUIZ : MonoBehaviour
+{
+    [SerializeField] private Text enunciado;
+    [SerializeField] private Text btn1text;
+    [SerializeField] private Text btn1text;
+    [SerializeField] private Text btn1text;
+    [SerializeField] private Text btn1text;
+    // Start is called before the first frame update
+    void Start()CDGFD
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
